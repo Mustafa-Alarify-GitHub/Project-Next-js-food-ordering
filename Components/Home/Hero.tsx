d0268333db78ba39695d4f557e0e12f8ab8166e0
Mustafa Al-arify <m2024u775516215@gmail.com>
2025-01-17 11:48:31 +0300

    add Hero component for displaying a prominent section with text and image

---
+++ Components/Home/Hero.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section>
+      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:items-center md:gap-8">
+          <div>
+            <div className="max-w-lg md:max-w-none">
+              <h2 className="text-2xl font-semibold text-gray-900 sm:text-3xl">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </h2>
+
+              <p className="mt-4 text-gray-700">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+                doloremque saepe architecto maiores repudiandae amet perferendis
+                repellendus, reprehenderit voluptas sequi.
+              </p>
+            </div>
+          </div>
+
+          <div>
+            <Image
+              width={500}
+              height={500}
+              src="/assets/Images/pizza.png"
+              className="rounded"
+              alt="ss"
+              loading="eager"
+              priority
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
