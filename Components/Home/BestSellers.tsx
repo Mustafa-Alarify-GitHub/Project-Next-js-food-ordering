13e0dfd8934f8b8d52612d2504af5c4e020f8ff9
Mustafa Al-arify <m2024u775516215@gmail.com>
2025-01-17 19:40:14 +0300

    feat: refactor BestSellers component to fetch data from server

--- Components/Home/BestSellers.tsx
+++ Components/Home/BestSellers.tsx
@@ -1,51 +1,13 @@
 import React from "react";
 import CardProduct from "../Global/CardProduct";
+import { getBestSellers } from "@/server/db/product";
 
-const BestSellers = () => {
-  const dataBestSellers = [
-    {
-      id: 1,
-      name: "Product 1",
-      image: "https://example.com/product1.jpg",
-      price: 19.99,
-      description:
-        "Product 1 is available for purchase at https https://example.com",
-    },
-    {
-      id: 1,
-      name: "Product 1",
-      image: "https://example.com/product1.jpg",
-      price: 19.99,
-      description:
-        "Product 1 is available for purchase at https https://example.com",
-    },
-    {
-      id: 1,
-      name: "Product 1",
-      image: "https://example.com/product1.jpg",
-      price: 19.99,
-      description:
-        "Product 1 is available for purchase at https https://example.com",
-    },
-    {
-      id: 1,
-      name: "Product 1",
-      image: "https://example.com/product1.jpg",
-      price: 19.99,
-      description:
-        "Product 1 is available for purchase at https https://example.com",
-    },
-  ];
+const BestSellers = async () => {
+  const dataBestSellers = await getBestSellers()
   return (
     <div className="w-full grid grid-cols-4 gap-4 mx-5">
       {dataBestSellers.map((el, i) => (
-        <CardProduct
-          description={el.description}
-          img={el.image}
-          name={el.name}
-          price={el.price}
-          key={i}
-        />
+        <CardProduct items={el} key={i} />
       ))}
     </div>
   );
