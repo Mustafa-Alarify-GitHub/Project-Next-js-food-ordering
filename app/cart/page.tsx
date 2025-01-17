cfbf9239835d04cdc1c1991a240e3dd990a61056
Mustafa Al-arify <m2024u775516215@gmail.com>
2025-01-17 19:39:27 +0300

    feat: implement Redux store and cart functionality with types and components

---
+++ app/cart/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const CartPage = () => {
+  return (
+    <div className=" w-full flex-col p-2 items-center">
+      <h5 className="text-primary">Cart</h5>
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
+        {/* Cart Items */}
+        <div className="w-full flex-col flex gap-2">
+        
+        </div>
+        {/* End Cart Items */}
+      </div>
+    </div>
+  );
+};
+
+export default CartPage;
