31bf2194e1f8f833fb3f4184b51fd42c56288901
Mustafa Al-arify <m2024u775516215@gmail.com>
2025-01-17 11:48:22 +0300

    add MinaTitle component for displaying titles with subtitles

---
+++ Components/Global/MinaTitle.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const MinaTitle = ({ title, subTitle }: { title: string; subTitle: string }) =>{
+  return (
+    <>
+    <span className='uppercase text-accent font-semibold leading-4'>
+      {subTitle}
+    </span>
+    <h2 className='text-primary font-bold text-4xl italic'>{title}</h2>
+  </>
+  )
+}
+
+export default MinaTitle
