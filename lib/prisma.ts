a221832d0a4b18bd4e8b85a55b4b73de39dcbe8b
Mustafa Al-arify <m2024u775516215@gmail.com>
2025-01-17 11:49:25 +0300

    add Prisma client initialization with environment-based logging

---
+++ lib/prisma.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client';
+
+export enum Environments {
+  DEV = 'development',
+  PROD = 'production',
+}
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log:
+      process.env.NODE_ENV === Environments.DEV
+        ? ['query', 'error', 'warn']
+        : ['error'],
+  });
+
+if (process.env.NODE_ENV !== Environments.PROD) globalForPrisma.prisma = db;
